e3f30dc68834b19389f7eed395c894146e5a15ff
Adelev <kak-dela1488@ukr.net>
2023-10-31 18:34:15 +0200

    addStoreAndFoodCost

--- snake-game/src/App.jsx
+++ snake-game/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
 import { Board, GameOver, Menu, Overlay } from './components';
+import { useSelector } from 'react-redux';
+import { selectSnakeScore } from './redux/snakeSlice';
 
 import './App.css';
 
 function App() {
     const [gameStatus, setGameStatus] = useState('game'); // menu | game | pause | gameOver
 
+    const snakeScore = useSelector(selectSnakeScore);
+    console.log(snakeScore);
     // useEffect(() => {
     //     console.log(gameStatus);
     // }, [gameStatus]);
